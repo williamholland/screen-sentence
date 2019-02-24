af8e9c3cb3c9493bc6144797408286b6a1e880d6
Will Holland <snowy.the.crow@gmail.com>
2019-02-24 10:25:13 +0000

    Refactor into a class
    to remove dependence on global variables

--- screen_sentence.py
+++ screen_sentence.py
@@ -12,10 +12,8 @@ logger.setLevel('DEBUG')
 
 pygame.init()
 
-screen = pygame.display.set_mode((600, 400), pygame.RESIZABLE)
 BLACK = 0,0,0
 WHITE = 255,255,255
-screen.fill(WHITE)
 
 TAGS = word_class.create_tags_from_csv('word_class.csv')
 
@@ -43,12 +41,12 @@ def get_font(text):
     '''
     screen_w, screen_h = pygame.display.get_surface().get_size()
     test_font = pygame.font.SysFont("LiberationSans", 10)
-    ratio = (test_font.size(TEXT)[0] / float(len(TEXT))) / 10.0
-    font_size = int(round(min((screen_w/ratio) / len(TEXT), screen_h )))
+    ratio = (test_font.size(text)[0] / float(len(text))) / 10.0
+    font_size = int(round(min((screen_w/ratio) / len(text), screen_h )))
     font = pygame.font.SysFont("LiberationSans", font_size)
 
-    x = int(round((screen_w - font.size(TEXT)[0]) / 2.0))
-    y = int(round(((screen_h - font.size(TEXT)[1]) / 2.0)))
+    x = int(round((screen_w - font.size(text)[0]) / 2.0))
+    y = int(round(((screen_h - font.size(text)[1]) / 2.0)))
     position = (x, y)
 
     return position, font
@@ -57,79 +55,109 @@ def log_tags(tags):
     pretty_tags = ['{word}<{tag}>'.format(word=word, tag=tag) if len(tag) > 1 else word for word, tag in tags]
     logger.info( ' '.join(pretty_tags) )
 
-CTRL_U = u'\x15'
 
-TEXT = ''
-def keydown(event):
-    ''' when a key is pressed '''
-    global TEXT
+class ScreenSentence(object):
 
-    if event.key == pygame.K_BACKSPACE:
-        if not TEXT:
-            return
-        TEXT = TEXT[:-1]
-    # ctrl+U to clear screen
-    elif event.unicode == CTRL_U:
-        TEXT = ''
-    else:
-        TEXT = TEXT + event.unicode
-
-    # log if end of a word
-    if re.match('[.!?]', event.unicode):
-        tokens = nltk.word_tokenize(TEXT)
-        tags = nltk.pos_tag(tokens)
-        log_tags(tags)
-
-    redraw()
-
-
-def resize(event):
-    global screen
-    logger.info('resize %s', event.size)
-    screen = pygame.display.set_mode(event.size, pygame.RESIZABLE)
-    redraw()
-
-
-def redraw():
-    global TEXT
-
+    screen = pygame.display.set_mode((600, 400), pygame.RESIZABLE)
     screen.fill(WHITE)
 
-    if not TEXT:
-        return
+    def clear_text(self, event):
+        ''' clear all text from the screen '''
+        self.text = ''
 
-    position, font = get_font(TEXT)
+    def backspace(self, event):
+        ''' remove last character '''
+        if self.text:
+            self.text = self.text[:-1]
 
+    KEYBINDINGS = {
+        pygame.K_BACKSPACE: backspace,
+    }
+
+    CTRL_KEYBINDINGS = {
+        pygame.K_u: clear_text,
+    }
+
+    _text = ''
     antialias = True
 
-    tokens = nltk.word_tokenize(TEXT)
-    tags = nltk.pos_tag(tokens)
+    @property
+    def text(self):
+        return self._text
 
-    # add back spaces that tokenize removed
-    words = []
-    total = ''
-    for i, token in enumerate(tokens):
-        if re.match(re.escape(total)+re.escape(token)+' ', TEXT):
-            token = token + ' '
-        words.append(token)
-        total = total+token
+    @text.setter
+    def text(self, value):
+        if value == self.text:
+            return
 
-    prev_word_end = position[0]
-    for word, tag in zip(words, tags):
-        label = font.render(word, antialias, word_colour(tag))
-        word_position = (prev_word_end, position[1])
-        screen.blit(label, word_position)
-        prev_word_end += font.size(word)[0]
+        self._text = value
+        self.redraw()
+
+        # log if end of a word
+        if re.match('[.!?]', event.unicode):
+            tokens = nltk.word_tokenize(self.text)
+            tags = nltk.pos_tag(tokens)
+            log_tags(tags)
+
+    def display_key(self, event):
+        ''' key pressed wants to be shown on the screen '''
+
+        self.text = self.text + event.unicode
+
+    def keydown(self, event):
+        ''' when a key is pressed '''
+
+        if event.key in self.KEYBINDINGS:
+            self.KEYBINDINGS[event.key](self, event)
+        elif event.mod & pygame.KMOD_CTRL and event.key in self.CTRL_KEYBINDINGS:
+            self.CTRL_KEYBINDINGS[event.key](self, event)
+        else:
+            self.display_key(event)
+
+    def resize(event):
+        logger.info('resize %s', event.size)
+        self.screen = pygame.display.set_mode(event.size, pygame.RESIZABLE)
+        self.redraw()
+
+    def redraw(self):
+
+        self.screen.fill(WHITE)
+
+        if not self.text:
+            return
+
+        position, font = get_font(self.text)
+
+        tokens = nltk.word_tokenize(self.text)
+        tags = nltk.pos_tag(tokens)
+
+        # add back spaces that tokenize removed
+        words = []
+        total = ''
+        for i, token in enumerate(tokens):
+            if re.match(re.escape(total)+re.escape(token)+' ', self.text):
+                token = token + ' '
+            words.append(token)
+            total = total+token
+
+        prev_word_end = position[0]
+        for word, tag in zip(words, tags):
+            label = font.render(word, self.antialias, word_colour(tag))
+            word_position = (prev_word_end, position[1])
+            self.screen.blit(label, word_position)
+            prev_word_end += font.size(word)[0]
+
+
+screen_sentence = ScreenSentence()
 
 
 EVENTS = {
     pygame.QUIT: exit,
-    pygame.KEYDOWN: keydown,
-    pygame.VIDEORESIZE: resize,
+    pygame.KEYDOWN: screen_sentence.keydown,
+    pygame.VIDEORESIZE: screen_sentence.resize,
 }
 
 
-clock = pygame.time.Clock()
 pygame.display.update()
 while True:
     for event in pygame.event.get():
